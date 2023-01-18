7248a20ef9a0f87dcb4fd72ffbee7227604b745c
RobCrawford527 <rcrawford527@hotmail.co.uk>
2023-01-18 11:02:27 +0000

    Updated output from peptide_intensity_distribution. Added ideas for calculation of means

--- R/mean_peptide_intensity_distribution.R
+++ R/mean_peptide_intensity_distribution.R
@@ -1,24 +1,53 @@
 mean_peptide_intensity_distribution <- function(data,
                                                 samples){
-  
-  
+
+
   for (i in names(data)){
-    
+
     data[[i]]
-    
-    
+
+
   }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-    
+
+
+  temp <- example_dist[sapply(example_dist,
+                              function(X) grepl("BY4741_Total_Untreated", X$condition) & grepl("RPS3", X$name))]
+  temp_combined <- merge(temp[[1]]$distribution,
+                         merge(temp[[2]]$distribution[,c(2,4)],
+                               temp[[3]]$distribution[,c(2,4)],
+                               by = "distance",
+                               all = TRUE),
+                         by = "distance",
+                         all = TRUE)
+  temp_combined$mean <- rowMeans(temp_combined[,4:6])
+  temp_combined$min <- temp_combined$mean - rowSds(as.matrix(temp_combined[,4:6]))
+  temp_combined$max <- temp_combined$mean + rowSds(as.matrix(temp_combined[,4:6]))
+
+
+
+
+
+  combined <- as.matrix(data.frame(rep1 = temp[[1]]$distribution[,"cumulative"],
+                                   rep2 = temp[[2]]$distribution[,"cumulative"],
+                                   rep3 = temp[[3]]$distribution[,"cumulative"]))
+  temp_combined <- data.frame(residue = temp[[1]]$distribution[,"residue"],
+                              distance = temp[[1]]$distribution[,"distance"],
+                              cumulative = rowMeans(combined, na.rm = TRUE),
+                              min = rowMeans(combined, na.rm = TRUE) - rowSds(combined, na.rm = TRUE),
+                              max = rowMeans(combined, na.rm = TRUE) + rowSds(combined, na.rm = TRUE))
+
+
+
+
+
+
+  temp_combined <- temp_combined[,c("residue",
+                                    "distance",
+                                    "mean",
+                                    "min",
+                                    "max")]
+  colnames(temp_combined)[colnames(temp_combined) == "mean"] <- "cumulative"
+
+
+
 }
--- R/peptide_intensity_distribution.R
+++ R/peptide_intensity_distribution.R
@@ -7,7 +7,7 @@ peptide_intensity_distribution <- function(data,
                                            end_col,
                                            len_col,
                                            sep = "/"){
-  
+
   # change column names
   colnames(data)[colnames(data) == names_col] <- "name"
   colnames(data)[colnames(data) == sam_col] <- "sample"
@@ -16,14 +16,14 @@ peptide_intensity_distribution <- function(data,
   colnames(data)[colnames(data) == start_col] <- "start"
   colnames(data)[colnames(data) == end_col] <- "end"
   colnames(data)[colnames(data) == len_col] <- "length"
-  
+
   # unite name and sample columns
   data <- tidyr::unite(data = data,
                        col = "name_sample",
                        name,
                        sample,
                        sep = sep)
-  
+
   # keep only rows and columns of interest
   data_1 <- dplyr::filter(.data = data[,c("name_sample",
                                           "sequence",
@@ -32,19 +32,19 @@ peptide_intensity_distribution <- function(data,
                                           "end",
                                           "length")],
                           value != 0)
-  
+
   # separate into list, one item for each name/sample combination
   data_2 <- list()
   for (i in unique(data_1[,"name_sample"])){
     data_2[[i]] <- dplyr::filter(.data = data_1,
                                  name_sample == i)
   }
-  
+
   ### intensity distributions
   # calculate cumulative peptide intensity distributions
   distribution <- lapply(data_2,
                          function(X) int_dist(X))
-  
+
   ### metadata
   # find protein length
   # calculate numbers of non-zero peptides
@@ -55,24 +55,39 @@ peptide_intensity_distribution <- function(data,
                     function(X) nrow(X))
   coverage <- lapply(distribution,
                      function(X) sum(X[,"intensity"] != 0))
-  
+
   # create empty list for output
   # write in metadata and distribution for each name/sample combination
   output <- list()
   for (i in unique(names(data_2))){
     # split name and sample
     name_sample <- strsplit(i, split = "/")[[1]]
-    
+
     # write output list
     # contains metadata vector and distribution data frame
-    output[[i]] <- list(metadata = c(name = name_sample[1],
-                                     sample = name_sample[2],
-                                     length = length[[i]],
-                                     nonzero_peptides = nonzero[[i]],
-                                     coverage = coverage[[i]] / length[[i]]),
+    output[[i]] <- list(name = name_sample[1],
+                        condition = gsub("_[0-9]+$",
+                                         "",
+                                         name_sample[2]),
+                        replicate = substr(name_sample[2],
+                                           start = nchar(name_sample[2]),
+                                           stop = nchar(name_sample[2])),
+                        length = length[[i]],
+                        nonzero_peptides = nonzero[[i]],
+                        coverage = coverage[[i]] / length[[i]],
                         distribution = distribution[[i]])
   }
-  
+
   # return output list
   output
 }
+
+example_dist <- peptide_intensity_distribution(data = example,
+                                               names_col = "GENENAME",
+                                               sam_col = "Sample",
+                                               seq_col = "Sequence",
+                                               val_col = "intensity",
+                                               start_col = "Start.position",
+                                               end_col = "End.position",
+                                               len_col = "protein_length",
+                                               sep = "/")
