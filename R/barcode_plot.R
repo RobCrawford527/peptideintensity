7b6ea5f5e57b56264cabc5c9e54c3759604e3f83
RobCrawford527 <rcrawford527@hotmail.co.uk>
2023-03-02 11:42:43 +0000

    Created barcode_plot and woods_plot functions.

---
+++ R/barcode_plot.R
@@ -0,0 +1,80 @@
+#' Plot Protein Barcodes For Proteins Of Interest
+#'
+#' @param data Data frame containing peptide differential abundance results
+#' @param protein_id Column containing protein IDs.
+#' @param proteins Protein(s) to plot. No limit, but more than 5 or so will look messy.
+#' @param comparison Column containing the comparison.
+#' @param comparisons Comparison(s) to plot. All by default.
+#' @param diff Column containing differential abundance data.
+#' @param start_position Column containing peptide start positions.
+#' @param end_position Column containing peptide end positions.
+#' @param protein_length Column containing protein lengths.
+#' @param colour Column containing colour indications. Should have three levels: "down", "unchanged" and "up".
+#'
+#' @return Barcode plots for the selected proteins and comparisons, indicating peptides that have changed in abundance between samples.
+#' @export
+#'
+#' @examples
+#' 
+barcode_plot <- function(data,
+                         protein_id,
+                         proteins,
+                         comparison,
+                         comparisons = NULL,
+                         diff,
+                         start_position,
+                         end_position,
+                         protein_length,
+                         colour){
+  
+  # define colours to use in plot
+  colours <- viridis::viridis(n = 2, 
+                              begin = 0.15,
+                              end = 0.85,
+                              direction = -1,
+                              option = "inferno")
+  colours <- c(colours[1], "grey75", colours[2])
+  
+  # change column names
+  colnames(data)[colnames(data) == protein_id] <- "id"
+  colnames(data)[colnames(data) == comparison] <- "comparison"
+  colnames(data)[colnames(data) == diff] <- "diff"
+  colnames(data)[colnames(data) == start_position] <- "start"
+  colnames(data)[colnames(data) == end_position] <- "end"
+  colnames(data)[colnames(data) == protein_length] <- "length"
+  colnames(data)[colnames(data) == colour] <- "colour"
+  
+  # filter data to keep only proteins and comparisons of interest
+  # remove peptides with NA values for diff
+  if (is.null(comparisons)){
+    comparisons <- unique(data[,"comparison"])
+  }
+  data <- dplyr::filter(data,
+                        id %in% proteins & !is.na(diff) & comparison %in% comparisons)
+  
+  # create plot
+  plot <- ggplot2::ggplot(data,
+                          mapping = ggplot2::aes(fill = colour)) +
+    ggplot2::geom_rect(mapping = ggplot2::aes(xmin = (start - 1) / length * 100,
+                                              xmax = end / length * 100,
+                                              ymin = -15,
+                                              ymax = 15),
+                       alpha = 0.8) +
+    ggplot2::scale_x_continuous(limits = c(0, 100),
+                                expand = c(0, 0),
+                                breaks = NULL) +
+    ggplot2::scale_y_continuous(limits = NULL,
+                                expand = c(0, 0),
+                                breaks = NULL) +
+    ggplot2::coord_equal(ratio = 1) +
+    ggplot2::scale_fill_manual(values = colours) +
+    ggplot2::facet_grid(comparison ~ id) +
+    ggplot2::theme_classic() +
+    ggplot2::theme(strip.background = ggplot2::element_blank(),
+                   panel.background = ggplot2::element_blank(),
+                   panel.border = ggplot2::element_rect(fill = NA),
+                   legend.position = "bottom")
+  
+  # return plot
+  plot
+}
