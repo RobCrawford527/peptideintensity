8df2d03e68de96eb0e836f1a404d41014cb5a4c4
RobCrawford527 <rcrawford527@hotmail.co.uk>
2023-03-14 11:58:47 +0000

    Update to add dplyr functionality.

--- R/peptide_intensity_distribution.R
+++ R/peptide_intensity_distribution.R
@@ -1,15 +1,15 @@
 #' Generate Cumulative Peptide Intensity Distributions
 #'
 #' @param data A data frame containing peptide-level proteomics data.
-#' @param names_col Column containing protein names.
-#' @param exp_col Column containing experiment.
-#' @param cond_col Column containing condition.
-#' @param rep_col Column containing replicate.
-#' @param seq_col Column containing peptide sequence.
-#' @param val_col Column containing peptide intensity data.
-#' @param start_col Column containing start residue of peptide.
-#' @param end_col Column containing end residue of peptide.
-#' @param len_col Column containing protein length.
+#' @param protein_id Column containing protein names.
+#' @param experiment Column containing experiment (i.e. sample + replicate).
+#' @param sample Column containing sample.
+#' @param replicate Column containing replicate.
+#' @param sequence Column containing peptide sequence.
+#' @param value Column containing peptide intensity data.
+#' @param start Column containing start residue of peptide.
+#' @param end Column containing end residue of peptide.
+#' @param length Column containing protein length.
 #' @param sep Separating character. Must not be present in either protein names
 #'     or experiment.
 #' @param threshold Threshold for number of peptides. Peptide intensity
@@ -24,52 +24,42 @@
 #' @examples
 #'
 peptide_intensity_distribution <- function(data,
-                                           names_col,
-                                           exp_col,
-                                           cond_col,
-                                           rep_col,
-                                           seq_col,
-                                           val_col,
-                                           start_col,
-                                           end_col,
-                                           len_col,
+                                           protein_id,
+                                           experiment,
+                                           sample,
+                                           replicate,
+                                           sequence,
+                                           value,
+                                           start,
+                                           end,
+                                           length,
                                            sep = "/",
                                            threshold = 1){
 
-  # change column names
-  colnames(data)[colnames(data) == names_col] <- "name"
-  colnames(data)[colnames(data) == exp_col] <- "experiment"
-  colnames(data)[colnames(data) == cond_col] <- "condition"
-  colnames(data)[colnames(data) == rep_col] <- "replicate"
-  colnames(data)[colnames(data) == seq_col] <- "sequence"
-  colnames(data)[colnames(data) == val_col] <- "value"
-  colnames(data)[colnames(data) == start_col] <- "start"
-  colnames(data)[colnames(data) == end_col] <- "end"
-  colnames(data)[colnames(data) == len_col] <- "length"
-
   # keep only rows and columns of interest
-  data_1 <- dplyr::filter(.data = data[,c("name",
-                                          "experiment",
-                                          "condition",
-                                          "replicate",
-                                          "sequence",
-                                          "value",
-                                          "start",
-                                          "end",
-                                          "length")],
-                          value != 0)
+  data_1 <- dplyr::filter(dplyr::select(data,
+                                        {{ protein_id }},
+                                        {{ experiment }},
+                                        {{ sample }},
+                                        {{ replicate }},
+                                        {{ sequence }},
+                                        {{ value }},
+                                        {{ start }},
+                                        {{ end }},
+                                        {{ length }} ),
+                          {{ value }} != 0)
 
   # separate into list by protein name, condition and replicate
   data_2 <- list()
-  for (i in unique(data_1[,"name"])){
-    data_1a <- dplyr::filter(.data = data_1,
-                             name == i)
-    for (j in unique(data_1a[,"condition"])){
-      data_1b <- dplyr::filter(.data = data_1a,
-                               condition == j)
-      for (k in unique(data_1b[,"replicate"])){
-        data_1c <- dplyr::filter(.data = data_1b,
-                                 replicate == k)
+  for (i in unique(dplyr::pull(data_1, {{ protein_id }} ))){
+    data_1a <- dplyr::filter(data_1,
+                             {{ protein_id }} == i)
+    for (j in unique(dplyr::pull(data_1a, {{ sample }} ))){
+      data_1b <- dplyr::filter(data_1a,
+                               {{ sample }} == j)
+      for (k in unique(dplyr::pull(data_1b, {{ replicate }} ))){
+        data_1c <- dplyr::filter(data_1b,
+                                 {{ replicate }} == k)
 
         # check if number of peptides exceeds threshold
         # write into list if it does
